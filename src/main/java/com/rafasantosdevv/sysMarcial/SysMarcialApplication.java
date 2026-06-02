b8579a96bc35b124732ac59bab37cb1825fbbfd7
Rafael <rafaeldosantos__@outlook.com>
2026-06-01 23:05:19 -0300

    Initial commit

---
+++ src/main/java/com/rafasantosdevv/sysMarcial/SysMarcialApplication.java
@@ -0,0 +1,13 @@
+package com.rafasantosdevv.sysMarcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysMarcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysMarcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafasantosdevv/sysMarcial/SysMarcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafasantosdevv.sysMarcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysMarcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
